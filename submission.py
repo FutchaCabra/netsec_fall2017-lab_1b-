6e97a07e9e2967742a00b9caf67155fe6afa11be
FutchaCabra <31625374+FutchaCabra@users.noreply.github.com>
2017-09-08 09:42:21 -0400

    Update submission.py

--- submission.py
+++ submission.py
@@ -1,7 +1,7 @@
 from playground.network.packet import PacketType
 from playground.network.packet.fieldtypes import UINT32, STRING, BUFFER, BOOL, ListFieldType
 class Access(PacketType):
-	def Access(): 
+	
 		DEFINITION_IDENTIFIER = "network.access"
 		DEFINITION_VERSION = "2.0"
 	
@@ -10,14 +10,8 @@ class Access(PacketType):
 			("data", BUFFER)
 			]
 	
-		packet1 = Access()
-		packet1.name = "RequestUserName"
-		packet1.data = "What is your name?"
-		
-		
-	
 class Username(PacketType): 
-	def Username():
+	
 		DEFINITION_IDENTIFIER = "network.Username"
 		DEFINITION_VERSION = "2.0"
 	
@@ -25,15 +19,9 @@ class Username(PacketType):
 			("title", STRING),
 			("data", BUFFER)
 			]	
-	
-		packet2 = Access()
-		packet2.title = "UserName"
-		packet2.data = "Sean Futch"
-		
-		
-	
+				
 class PasswordRequest(PacketType): 
-	def PasswordRequest():
+	
 		DEFINITION_IDENTIFIER = "network.PasswordRequest"
 		DEFINITION_VERSION = "2.0"
 	
@@ -42,59 +30,56 @@ class PasswordRequest(PacketType):
 			("data", BUFFER)
 			]	
 	
-		packet3 = Password()
-		packet3.Request = "Password Request"
-		packet3.data = "Please Enter Password."
-		
-		
 	
 class UserPassword(PacketType):
-	def UserPassword(): 
+	
 		DEFINITION_IDENTIFIER = "network.UserPassword"
 		DEFINITION_VERSION = "2.0"
 	
 		FIELDS = [
 			("data", BUFFER)
 			]	
-	
-		packet4 = UserPassword()
-		packet4.data = "Password"
-	
-	# deserializer = Access.Deserializer()
-	
-	def basicUnitTest():
-		packet1 = Access()
-		packet1Bytes = packet1.__serialize__()
-		print (packet1)
-		packet1a = Access.Deserialize(packet1Bytes)
-		print (packet1a)
-		assert packet1 == packet1a
+
+
+
+def basicUnitTest():
+	packet1 = Access()
+	packet1.name = "RequestUserName"
+	packet1.data = (b"What is your name?")
+	packet1Bytes = packet1.__serialize__()
+	packet1a = Access.Deserialize(packet1Bytes)
+	print (Access)
+	print (packet1a)
+	assert packet1 == packet1a
 		
-		packet2 = Username()
-		packet2Bytes = packet2.__serialize__()
-		packet2a = Username.Deserialize(packet2Bytes)
-		assert packet2 == packet2a
+	packet2 = Username()
+	packet2.title = "UserName"
+	packet2.data = (b"Sean Futch")
+	packet2Bytes = packet2.__serialize__()
+	packet2a = Username.Deserialize(packet2Bytes)
+	print (Username)
+	print (packet2a)
+	assert packet2 == packet2a
 		
-		packet3 = PasswordRequest()
-		packet3Bytes = packet3.__serialize__()
-		packet3a = PasswordRequest.Deserialize(packet3Bytes)
-		assert packet3 == packet3a
+	packet3 = PasswordRequest()
+	packet3.Request = "Password Request"
+	packet3.data = (b"Please Enter Password.")
+	packet3Bytes = packet3.__serialize__()
+	packet3a = PasswordRequest.Deserialize(packet3Bytes)
+	print (PasswordRequest)
+	print (packet3a)
+	assert packet3 == packet3a
 		
-		packet4 = UserPassword()
-		packet4Bytes = packet4.__serialize__()
-		packet4a = UserPassword.Deserialize(packet4Bytes)
-		assert packet4 == packet4a
+	packet4 = UserPassword()
+	packet4.data = (b"Password")
+	packet4Bytes = packet4.__serialize__()
+	packet4a = UserPassword.Deserialize(packet4Bytes)
+	print (UserPassword)
+	print (packet4a)
+	assert packet4 == packet4a
 		
-	if __name__=="__main__":
-		basicUnitTest()
+if __name__=="__main__":
+	basicUnitTest()
 		
 		
-	print ("Basic Unit Test Completed")
-		
-	
-	#for packet in deserializer.nextPackets():
-	#	print ("got a packet")
-	#	if packet == packet1: print ("It's packet 1!") 
-	#	elif packet == packet2: print ("It's packet 2!") 
-	#	elif packet == packet3: print ("It's packet 3!")
-	#	elif packet == packet4: print ("It's packet 4!")
+print ("Basic Unit Test Completed")
